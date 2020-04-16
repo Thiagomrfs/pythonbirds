bac85a9b0f668f323747ca517041aa3b0c01a0fa
Thiagomrfs <thiagomarbsn@gmail.com>
2020-04-15 22:51:13 -0300

    Resolvendo bugs de mecânica

---
+++ pacote/modulo.py
@@ -0,0 +1,11 @@
+def soma(*args):
+    total = 0
+    for n in args:
+        total +=
+    return total
+
+print(soma(1))
+print(soma(1, 2))
+print(soma(1, 2, 3))
+print(soma(1, 2, 3, 4))
+print(soma(1, 2, 3, 4, 5))
